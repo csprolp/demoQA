c401049ad0cb13775b8cac93e843e7ea620741bf
Vyacheslav Yushmanov <csprolp@gmail.com>
2025-04-11 23:59:51 +0300

    add jenkins and allure

--- hw1/tests/conftest.py
+++ hw1/tests/conftest.py
@@ -2,9 +2,41 @@ import pytest
 from selenium import webdriver
 from selene import browser
 
+from utils import attach
+from selenium import webdriver
+from dotenv import load_dotenv
+from selenium.webdriver.chrome.options import Options
+import os
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
+selenoid_login = os.getenv("SELENOID_LOGIN")
+selenoid_pass = os.getenv("SELENOID_PASS")
+selenoid_url = os.getenv("SELENOID_URL")
+
 
 @pytest.fixture(scope="function", autouse=True)
 def config_browser():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "128.0",
+        "selenoid:options": {"enableVNC": True, "enableVideo": True},
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        options=options,
+    )
+
+    browser.config.driver = driver
+
+    browser.config.driver = driver
     browser.config.base_url = "https://demoqa.com"
     browser.config.window_width = 1800
     browser.config.window_height = 1200
@@ -12,5 +44,9 @@ def config_browser():
     driver_options.page_load_strategy = "eager"
     browser.config.driver_options = driver_options
 
-    yield
+    yield browser
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
     browser.quit()
