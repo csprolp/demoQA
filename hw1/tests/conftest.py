eb9f648bb7243befa6ee70018269e44dc7f5e468
Vyacheslav Yushmanov <csprolp@gmail.com>
2025-04-12 00:53:48 +0300

    fix conftest

--- hw1/tests/conftest.py
+++ hw1/tests/conftest.py
@@ -5,23 +5,27 @@ from selenium import webdriver
 from dotenv import load_dotenv
 import os
 from selenium.webdriver.chrome.options import Options
+from pathlib import Path
 
 
 from utils.attach import add_html, add_logs, add_video, add_screenshot
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(
+    scope="session",
+    autouse=True,
+)
 def load_env():
-    load_dotenv()
-
-
-selenoid_login = os.getenv("SELENOID_LOGIN")
-selenoid_pass = os.getenv("SELENOID_PASS")
-selenoid_url = os.getenv("SELENOID_URL")
+    env_path = Path(__file__).parent.parent.parent / ".env"  # Путь к корню проекта
+    load_dotenv(env_path)
+    print("SELENOID_URL:", os.getenv("SELENOID_URL"))
 
 
 @pytest.fixture(scope="function", autouse=True)
 def config_browser():
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -30,6 +34,9 @@ def config_browser():
     }
 
     options.capabilities.update(selenoid_capabilities)
+    print(
+        f"Connecting to: https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub"
+    )
     driver = webdriver.Remote(
         command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
         options=options,
